accc86caf4d6f62a357a43434cb3888b21ed97e9
kokimame <kokimametani@gmail.com>
2017-10-16 18:46:50 +0900

    Remove FrameManager from Framelist

--- gui/framelist.py
+++ gui/framelist.py
@@ -1,41 +1,14 @@
 
 from gui.qt import *
-from gui.bundle import BundleItemUi, BundleUi
-
 # TODO: Move Bundle class into gui.bundle
 from bavl.bundle import Bundle
 
 # TODO: Rename to Frame
 class FrameList(QListWidget):
 
-    class FrameManager():
-        def initBundle(self, name, names, index):
-            # Do some setup for bundles using "Frame preference"
-            # before its initialization
-            if name in names:
-                raise Exception("Exception: Bundle with name %s already exists." % name)
-            return Bundle(name, index)
-
-        # This should be done by ID not name
-        def setItemByName(self, name, item):
-            for bundle in self._Frame:
-                if bundle.name == name:
-                    bundle.updateItems(item)
-                    return
-            raise Exception("Error: Bundle with name '%s' is not found in the Frame" % name)
-
-        def remove(self, bundle):
-            self._Frame.remove(bundle)
-
-        def getBundleNames(self):
-            return [bundle.name for bundle in self._Frame]
-
-
     def __init__(self, mw, parent=None):
         super(FrameList, self).__init__(parent)
         self.mw = mw
-        # Fixme: Framelist become frame itself. No need to store ids
-        self.currentIds = []
         self.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.setStyleSheet("""
                             QListWidget::item { border-bottom: 1px solid black; }
@@ -44,7 +17,8 @@ class FrameList(QListWidget):
 
     def add(self, name):
         if name in self.getCurrentNames():
-            raise Exception("Exception: Bundle with name %s already exists." % name)
+            print("Bundle with name %s already exists." % name)
+            return
         self.addBundle(Bundle(name, self.count() + 1))
 
     def updateBundle(self, name, items):
@@ -54,33 +28,16 @@ class FrameList(QListWidget):
                 return
         raise Exception("Error: Bundle with name '%s' is not found in the Frame" % name)
 
-    def remove(self):
-        pass
 
     def addBundle(self, bundle):
         bui, bitem = self.mw.bdfactory.createUi(self.count() + 1, bundle, parent=self)
         self.addItem(bui)
         self.setItemWidget(bui, bitem)
-        # Fixme: Use real ID to check existing bundles in the list
-        self.setNewId(bundle.name)
-
-    def setNewBundles(self):
-        # TODO: Move FM out of the class and make two classes communicate with each other(?)
-        # TODO: Or delete FM class at all and make FrameList do everything.
-        # like Signal & Slot (?)
-        # Check newly created bundles in the frame of the FrameManager
-        newbds = self.fm.getNewBundles(self.currentIds)
-
-        for bundle in newbds:
-            self.addBundle(bundle)
-
 
     def deleteSelectedUi(self):
         for bui in self.selectedItems():
             bitem = self.itemWidget(bui)
             self.takeItem(bitem.index - 1)
-            self.currentIds.remove(bitem.name)
-            self.fm.remove(bitem.bundle)
             self.updateIndex(bitem.index - 1)
 
     def updateIndex(self, n):
@@ -105,9 +62,6 @@ class FrameList(QListWidget):
         for i in range(self.count()):
             bitem = self.getWidgetItem(i)
             bitem.updateMode(newMode)
-
-    def setNewId(self, id):
-        self.currentIds.append(id)
 
     def getCurrentNames(self):
         return [self.getWidgetItem(i).name for i in range(self.count())]

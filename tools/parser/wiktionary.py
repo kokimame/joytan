4492cda63e16c9e4eb00e562b7331d9a66048881
kokimame <kokimametani@gmail.com>
2017-10-22 20:17:58 +0900

    Added simple wiktionary parser

--- tools/parser/wiktionary.py
+++ tools/parser/wiktionary.py
@@ -7,3 +7,23 @@ class WiktionaryParser(BaseParser):
     def __init__(self):
         BaseParser.__init__(self)
         self.source = "https://en.wiktionary.org/wiki/"
+
+    def run(self, data):
+        # Dictionary which stores definition and example
+        defex = []
+        soup = BeautifulSoup(data, "html.parser")
+        table = soup.find('ol')
+
+        for content in table.find_all('li', recursive=False):
+            examples = []
+            for trash in content.find_all('ul'):
+                trash.replaceWith('')
+            for example in content.find_all('dl'):
+                examples.append(example.text)
+                # Remove stored example from definition text
+                example.replaceWith('')
+            if len(examples) == 0:
+                examples = ['']
+            defex.append({'define': content.text, 'examples':examples})
+
+        return defex

f7b888b92353f8e91b9f34b1fb7889b9da6562ec
kokimame <kokimametani@gmail.com>
2017-11-01 23:36:20 +0900

    Use ffmpeg on Windows

--- tools/cmder/__init__.py
+++ tools/cmder/__init__.py
@@ -5,12 +5,14 @@ import shutil
 
 if isMac or isLin:
     assert shutil.which("cat") != None, "cat command not found."
+    assert shutil.which("grep") != None, "grep command not found"
     assert shutil.which("ffmpeg") != None, "ffmpeg command not found."
     assert shutil.which("sox") != None, "sox command not found."
 
 if isWin:
     # Fixme: How to know whether users have type command on Windows
     #assert shutil.which("type") != None, "type command not found"
+    assert shutil.which("ffmpeg") != None, "ffmpeg command not found."
     pass
 
 

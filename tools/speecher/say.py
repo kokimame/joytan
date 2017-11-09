3c358564b3a0d7294365d02bdfc20e76f6331035
kokimame <kokimametani@gmail.com>
2017-11-10 01:00:29 +0900

    Say command previewing

--- tools/speecher/say.py
+++ tools/speecher/say.py
@@ -2,6 +2,24 @@ from tools.speecher import *
 
 
 class Say(BaseSpeecher):
+    langPpl = {'en': 'Alex',
+               'ja': 'Kyoko ',
+               'ko': 'Yuna',
+               'it': 'Alice',
+               'sv': 'Alva',
+               'fr': 'Thomas',
+               'de': 'Anna',
+               'zh-cn': 'Sin-ji',
+               'hi': 'Lekha',
+               'ru': 'Milena',
+               'ar': 'Maged',
+               'th': 'Kanya',
+               'id': 'Damayanti',
+               'he': 'Carmit',
+               'sk': 'Laura',
+               'eo': 'Monica'}
+
+
     def dictate(self, script, lang=None, output=None):
         print("Script: %s (%s)" % (script, lang))
 
@@ -9,31 +27,21 @@ class Say(BaseSpeecher):
             os.makedirs(os.path.dirname(output), exist_ok=True)
             self.save(script, lang=lang, output=output)
 
+        else:
+            ppl = self.code2ppl(lang)
+            call('say %s "%s"' % (ppl, script), shell=True)
+
     def save(self, script, lang=None, output=None):
-        langVersion = {'en': 'Alex',
-                       'ja': 'Kyoko ',
-                       'ko': 'Yuna',
-                       'it': 'Alice',
-                       'sv': 'Alva',
-                       'fr': 'Thomas',
-                       'de': 'Anna',
-                       'zh-cn': 'Sin-ji',
-                       'hi': 'Lekha',
-                       'ru': 'Milena',
-                       'ar': 'Maged',
-                       'th': 'Kanya',
-                       'id': 'Damayanti',
-                       'he': 'Carmit',
-                       'sk': 'Laura',
-                       'eo': 'Monica'}
-
-        try:
-            lver = '-v ' + langVersion[lang]
-        except (KeyError):
-            lver = ''
-            print("Unsupported language detected: %s" % lang)
-
+        ppl = self.code2ppl(lang)
         cmd = 'say %s "%s" -o %s.aiff;' \
               'ffmpeg -loglevel panic -i %s.aiff -ac 2 -acodec libmp3lame -ar 44100 -ab 64k -f mp3 %s.mp3' \
-              % (lver, script, output, output, output)
+              % (ppl, script, output, output, output)
         call(cmd, shell=True)
+
+    def code2ppl(self, lang):
+        try:
+            ppl = '-v ' + self.langPpl[lang]
+        except (KeyError):
+            ppl = ''
+            print("Unsupported language detected: %s" % lang)
+        return ppl

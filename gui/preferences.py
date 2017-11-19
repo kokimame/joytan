cd0c7aed0ec4532ae2189666469dac59150f0a85
kokimame <kokimametani@gmail.com>
2017-11-19 22:53:32 +0900

    Added mapping widgets between tts voice and languages

--- gui/preferences.py
+++ gui/preferences.py
@@ -2,6 +2,46 @@ from gui.qt import *
 import gui
 from tools.parser import Parsers
 from tools.speecher import Speechers
+from gui.utils import LANGUAGES, LANGCODES
+
+class VlMapWidget(QWidget):
+    # Maps between Voice and Language and play sample text-to-speech
+    def __init__(self, tts, label, langCode):
+        super(VlMapWidget, self).__init__()
+        self.tts = Speechers[tts]
+        self.label = label
+        self.langCode = langCode
+        self.initUi()
+
+    def initUi(self):
+        lbl = QLabel('%s:' % self.label.title())
+        self.langCombo = QComboBox()
+        self.langCombo.addItems(sorted([lang.title() for lang in LANGUAGES.values()]))
+        self.langCombo.setCurrentText(LANGUAGES[self.langCode].title())
+        self.langCombo.currentTextChanged.connect(self.updateVoiceCombo)
+        lbl2 = QLabel('---> TTS:')
+        self.voiceCombo = QComboBox()
+        self.voiceCombo.addItems([name for name in self.tts.code2Name[self.langCode]])
+        self.testBtn = QPushButton('Test')
+        self.testBtn.clicked.connect(self.testVoice)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(lbl)
+        hbox.addWidget(self.langCombo)
+        hbox.addWidget(lbl2)
+        hbox.addWidget(self.voiceCombo)
+        hbox.addWidget(self.testBtn)
+        self.setLayout(hbox)
+
+    def updateVoiceCombo(self):
+        self.langCode = LANGCODES[self.langCombo.currentText().lower()]
+        self.voiceCombo.clear()
+        self.voiceCombo.addItems([name for name in self.tts.code2Name[self.langCode]])
+        self.voiceCombo.repaint()
+
+    def testVoice(self):
+        self.tts().preview(self.tts.code2Name[self.langCode][self.voiceCombo.currentText()])
+
 
 class Preferences(QDialog):
 
@@ -19,6 +59,7 @@ class Preferences(QDialog):
         self.setupEditors()
         self.setupCombo()
         self.setupButtons()
+        self.setupList()
 
     def setTab(self, tab):
         # Set by the absolute index of a tab based
@@ -34,19 +75,28 @@ class Preferences(QDialog):
         sc.setCurrentText(self.mw.pref['onlineSrc'])
         tc.addItems(sorted([site for site in Speechers.keys()]))
         tc.setCurrentText(self.mw.pref['tts'])
-        self.setVoiceCombo(self.mw.pref['tts'])
-        tc.currentTextChanged.connect(lambda: self.setVoiceCombo(tc.currentText()))
-
-    def setVoiceCombo(self, newTts):
-        form = self.form
-        form.voiceCombo.addItems(sorted(Speechers[newTts].voiceCombo))
 
     def setupButtons(self):
         form = self.form
         form.buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.onOk)
-        # It's pre'listen' though...
-        form.previewBtn.clicked.connect(lambda: Speechers[form.ttsCombo.currentText()]().
-                                               preview(form.voiceCombo.currentText()))
+
+
+    def setupList(self):
+        testList = self.form.testList
+        # Sort items in the order of 'name', 'def-x' and 'ex-x-x'
+        for row in sorted(sorted(list(self.mw.framelist.setting.langMap.keys())),
+                          key=lambda x: ['n', 'd', 'e'].index(x[0])):
+            langCode = self.mw.framelist.setting.langMap[row]
+            if langCode:
+                wig = VlMapWidget(self.mw.pref['tts'], row, langCode)
+            else:
+                # Temporally we consider English as default
+                wig = VlMapWidget(self.mw.pref['tts'], row, 'en')
+
+            lwi = QListWidgetItem()
+            lwi.setSizeHint(wig.sizeHint())
+            testList.addItem(lwi)
+            testList.setItemWidget(lwi, wig)
 
     def setupSpins(self):
         form = self.form

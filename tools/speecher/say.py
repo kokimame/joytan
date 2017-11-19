fd67762a77a925140e6969e6074e71a8e1e72e73
kokimame <kokimametani@gmail.com>
2017-11-19 23:34:39 +0900

    Made code2Names

--- tools/speecher/say.py
+++ tools/speecher/say.py
@@ -1,14 +1,29 @@
 from tools.speecher import *
+from gui.utils import LANGUAGES
 
+def getTtsHelp():
+    # Returns ['langCode', 'Voice name', 'Voice name + detail langCode (Shown in Combobox)', 'example']
+    i1 = str(check_output("say -v ?", shell=True), 'utf-8').strip().split('\n')
+    i2 = [' '.join(row.split()) for row in i1]
+    i3 = []
+    for i in i2:
+        combo, examp = i.split(' # ')
+        i4 = combo.split(' ')
+        if len(i4) == 2:
+            name, langCode = i4[0], i4[1].split('_')[0]
+            if langCode == 'zh':
+                langCode = i4[1].lower().replace('_', '-')
+        elif len(i4) == 3:
+            name, langCode = ' '.join([i4[0], i4[1]]), i4[2].replace('_', '-').split('-')[0]
+        else:
+            raise Exception("Unexpected voice name found on Mac's say: ", i4)
 
-def getVoiceInfo():
-    info = str(check_output("say -v ?", shell=True), 'utf-8').strip().split('\n')
-    info = [' '.join(row.split()) for row in info]
-    vd = {}
-    for line in info:
-        combo, example = line.split(' # ')
-        vd[combo] = example
-    return vd
+        i4[0], i4[1] = langCode, name
+        i4.append(combo)
+        i4.append(examp)
+        i3.append(i4)
+
+    return i3
 
 
 class Say(BaseSpeecher):
@@ -17,25 +32,14 @@ class Say(BaseSpeecher):
     from gui.utils import isMac
     if isMac:
         # voiceCombo has voice names shown in a combobox on Preferences
-        voiceInfo = getVoiceInfo()
-        voiceCombo = voiceInfo.keys()
-        code2Names = {}
-        for vc in voiceCombo:
-            vcs = vc.split(' ')
-            if len(vcs) == 2:
-                name, langCode = vcs[0], vcs[1].split('_')[0]
-                if langCode == 'zh':
-                    langCode = vcs[1].lower().replace('_', '-')
-            elif len(vcs) == 3: # 2 words voice name such as 'Good News' and 'Pipe organ'.
-                name, langCode = ' '.join([vcs[0], vcs[1]]), vcs[2].replace('_', '-').split('-')[0]
-            else:
-                raise Exception("Unexpected voice name found on Mac's say: ", vcs)
+        infos = getTtsHelp()
+        code2Names = {key: {'Not Available': None} for key in LANGUAGES}
 
-            try:
-                code2Names[langCode].append(name)
-            except KeyError:
-                code2Names[langCode] = [name]
-        print(code2Names)
+        for info in infos:
+            if info[0] in code2Names:
+                code2Names[info[0]][info[2]] = info[1]
+                if 'Not Available' in code2Names[info[0]]:
+                    del code2Names[info[0]]['Not Available']
 
 
     def dictate(self, script, langCode=None, output=None):
@@ -44,10 +48,8 @@ class Say(BaseSpeecher):
         if output:
             os.makedirs(os.path.dirname(output), exist_ok=True)
             self.save(script, output, langCode=langCode)
-
         else:
-            ppl = self.selectName(langCode)
-            call('say %s "%s"' % (ppl, script), shell=True)
+            call('say -v {name} "{script}"'.format(name=langCode, script=script), shell=True)
 
     def save(self, script, output, langCode=None):
         ppl = self.selectName(langCode)
@@ -67,9 +69,5 @@ class Say(BaseSpeecher):
         return ppl
 
     # Actually pre'listen' though.
-    def preview(self, combo):
-        script = self.voiceInfo[combo]
-        combo = combo.split(' ')
-        name = ' '.join(combo[0: len(combo) - 1])
-        lang = combo[-1].split('_')
-        call('say -v {name} "{script}"'.format(name=name, script=script), shell=True)
+    def preview(self, langCode, script='If any proper sample sentence is not found, I read this.'):
+        self.dictate(script, langCode=langCode)

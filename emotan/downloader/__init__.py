087f7ec2b4edec9da20425c7db51871bb3dde143
kokimame <kokimametani@gmail.com>
2017-12-25 23:54:19 +0900

    Excluded developing downloaders from combobox

--- emotan/downloader/__init__.py
+++ emotan/downloader/__init__.py
@@ -1,11 +1,7 @@
 from emotan.downloader.dictionarycom import DictionaryComDownloader
-from emotan.downloader.cambridge import CambridgeDownloader
-from emotan.downloader.oxford import OxfordDownloader
 from emotan.downloader.wiktionary import WiktionaryDownloader
 
 Downloaders = {
     "Dictionary.com": DictionaryComDownloader,
-    "Cambridge Dictionary": CambridgeDownloader,
-    "Oxford English Dictionary": OxfordDownloader,
-    "Wiktionary": WiktionaryDownloader
+    "Wiktionary": WiktionaryDownloader,
 }

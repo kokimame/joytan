f948e683d1bafe6aef99daa2fe45e4131d87775a
kokimame <kokimametani@gmail.com>
2018-01-02 10:58:42 +0900

    fixed test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,6 +1,5 @@
 from .gui_testing import joytan_running
-from gui.copy import CopyDialog
 
 def test_initialization():
     with joytan_running() as joytan_app:
-        joytan_app
+        print(dir(joytan_app))

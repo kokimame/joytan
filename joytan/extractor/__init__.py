9c4841cc2324201a03139cf212de30c8cf73e16f
kokimame <kokimametani@gmail.com>
2018-01-30 17:41:48 +0900

    Delete files related to deprecated feature

--- joytan/extractor/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017-Present: Koki Mametani <kokimametani@gmail.com>
-# License: GPLv3 or later; http://www.gnu.org/licenses/gpl.html
-
-from joytan.extractor.newline import NewlineExtractor
-
-Extractors = (
-    ("DSV format - Words separated by newline (*.dsv *.txt)", NewlineExtractor),
-)
